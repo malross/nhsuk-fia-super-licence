f9349cc2f7bee774ad360f09f8cafa148c5ac25e
Mal Ross <malross@gmail.com>
2026-01-20 16:42:42 +0000

    feat: Choose between renewing and applying for a new licence
    From here, it'll essentially be two entirely separate flows, possibly even with separate completion pages. When you have already held a super licence, it can be pretty simple to renew it unless you've been out of F1 for a while. Even then, you can appeal to be let back in without needing 40 super licence points.

--- app/routes.js
+++ app/routes.js
@@ -5,4 +5,20 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+// Check that one of the application types was selected and redirect accordingly
+router.post('/apply-or-renew-answer', function (req, res) {
+    var applicationType = req.session.data['apply-or-renew']
+
+    req.session.data['errors'] = {}
+    if (applicationType == "renew") {           // renewing licence
+        res.redirect('/most-recent-f1-season')
+    } else if (applicationType == "apply") {    // applying for a new licence
+        res.redirect('/grade-a-check')
+    } else {                                    // not answered, so show an error
+        req.session.data['errors'] = {
+            'not-answered': true
+        }
+        res.redirect('/apply-or-renew')
+    }
+})
 module.exports = router
